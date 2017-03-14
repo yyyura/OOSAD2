8482ebcf152afe0f454136b0970a82b3cc4d4456
yyyura <yyyura@gmail.com>
2017-03-14 13:55:38 +0000

    Singleton start

---
+++ week6/Singleton.java
@@ -0,0 +1,16 @@
+public class Singleton {
+
+	//static member
+	private static Singleton singleton;
+
+	//private constructor	
+	private Singleton() {}
+
+	//staic method
+	public static Singleton getInstance() {
+		if(singleton == null){
+			singleton = new Singleton();
+			}		
+	}
+
+}
